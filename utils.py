367e88d494cfa47f853255a997f3e85358b65f2c
AsokTamang <ashoktmg2058@gmail.com>
2026-04-17 14:16:16 -0400

    utils

---
+++ utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from numpy import loadtxt
+
+
+def normalizeRatings(Y, R):
+    Ymean = (np.sum(Y * R, axis=1) / (np.sum(R, axis=1) + 1e-12)).reshape(-1,
+                                                                          1)  # here we are finding the average rating of the movie
+    Ynorm = Y - np.multiply(Ymean, R)  # inorder to prevent the negative rating value if the user didnot rate the movie
+    return (Ynorm, Ymean)
+
+
+def load_precalc_params_small():
+    file = open('./data/small_movies_X.csv', 'rb')
+    X = loadtxt(file, delimiter=",")
+
+    file = open('./data/small_movies_W.csv', 'rb')
+    W = loadtxt(file, delimiter=",")
+
+    file = open('./data/small_movies_b.csv', 'rb')
+    b = loadtxt(file, delimiter=",")
+    b = b.reshape(1, -1)
+    num_movies, num_features = X.shape
+    num_users, _ = W.shape
+    return (X, W, b, num_movies, num_features, num_users)
+
+
+def load_ratings_small():
+    file = open('./data/small_movies_Y.csv', 'rb')
+    Y = loadtxt(file, delimiter=",")
+
+    file = open('./data/small_movies_R.csv', 'rb')
+    R = loadtxt(file, delimiter=",")
+    return (Y, R)
+
+
+def load_Movie_List_pd():
+
+    df = pd.read_csv('./data/small_movie_list.csv', header=0, index_col=0, delimiter=',', quotechar='"')
+    mlist = df["title"].to_list()
+    return (mlist, df)
+
+
+
+
+
